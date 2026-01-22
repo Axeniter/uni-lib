a01fb5d87921268ab49d654607138dd10fa863f3
Axeniter <anamievoid@gmail.com>
2026-01-22 22:50:45 +1000

    Feat: user schemas

---
+++ server/src/schemas/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+from uuid import UUID
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    username: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: UUID
+    email: EmailStr
+    username: str
+
+    class Config:
+        from_attributes = True
