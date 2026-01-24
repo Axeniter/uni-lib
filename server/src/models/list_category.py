23ae66a55e5063c601596b433a9a6a08b94df488
Axeniter <anamievoid@gmail.com>
2026-01-24 17:22:55 +1000

    Feat: list category orm

--- server/src/models/list_category.py
+++ server/src/models/list_category.py
@@ -6,7 +6,7 @@ class ListCategory(Base):
     __tablename__ = "list_categories"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False, index=True)
+    name = Column(String(100), nullable=False)
     order = Column(Integer, default=0, nullable=False)
     list_id = Column(Integer, ForeignKey("items_lists.id", ondelete="CASCADE"), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
